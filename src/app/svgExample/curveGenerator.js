bf51d518786f94c0a5270e8e6f881eb06905d1e0
alienfetuseater <alienfetuseater@gmail.com>
2020-07-24 00:12:37 -0400

    working on correcting for variable amplitude

--- src/app/svgExample/curveGenerator.js
+++ src/app/svgExample/curveGenerator.js
@@ -3,8 +3,8 @@ export default function CurveGenerator(lead, canvasWidth, canvasHeight) {
 	const xmlns = 'http://www.w3.org/2000/svg'
 	const NMBRHORIZSMLBXS = 275
 	let horizontalMM = canvasWidth / NMBRHORIZSMLBXS
-	// const NMBRVERTSMLBXS = 212
-	// let verticalMM = canvasHeight / NMBRVERTSMLBXS
+	const NMBRVERTSMLBXS = 212
+	let verticalMM = canvasHeight / NMBRVERTSMLBXS
 	// const NMBRHORIZLRGBXS = 55
 	// const NMBRVERTLRGBXS = 42
 	// let horizontalBB = horizontalMM * 5
@@ -17,7 +17,16 @@ export default function CurveGenerator(lead, canvasWidth, canvasHeight) {
 	let origin_y = end_y
 	let left_bound = lead.domain[0] * horizontalMM
 	let right_bound = lead.domain[1] * horizontalMM
-	// let amplitude =
+
+	// this is to try and adjust amplitude for given desired_amplitude of pwave
+	let X = right_bound
+	let original_height = eval(curve)
+	let desired_amplitude = 5 // for 5 mm tall p wave
+	let corrected_desired_amplitude = desired_amplitude * verticalMM
+	let amplitude_multiplier = corrected_desired_amplitude / original_height
+
+	// now need to correct for vertical shift
+	let vertical_shift = eval(curve) * amplitude_multiplier
 
 	const draw = function () {
 		for (let x = left_bound; x <= right_bound; x += 0.1) {
@@ -25,11 +34,13 @@ export default function CurveGenerator(lead, canvasWidth, canvasHeight) {
 
 			let X = x
 			let x1 = X + origin_x
-			let y1 = eval(curve) + origin_y
+			let y1 =
+				eval(curve) * amplitude_multiplier + origin_y - vertical_shift
 
 			X += 0.1
 			let x2 = X + origin_x
-			let y2 = eval(curve) + origin_y
+			let y2 =
+				eval(curve) * amplitude_multiplier + origin_y - vertical_shift
 
 			line.setAttributeNS(null, 'x1', x1)
 			line.setAttributeNS(null, 'y1', y1)
