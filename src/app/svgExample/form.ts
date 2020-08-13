e05e365f1713b00c9a1dd27b3584cc4de156a3c9
alienfetuseater <alienfetuseater@gmail.com>
2020-08-12 22:09:47 -0400

    started making form for user input to alter ecg behavior

---
+++ src/app/svgExample/form.ts
@@ -0,0 +1,8 @@
+export default function sideForm() {
+	// create an array of data defining buttons and purposess
+	const constructor = () => {
+		// create forms and buttons, place absolutely so doesnt mess with placement of ecg paper
+		const main = document.querySelector('main')
+	}
+	return {}
+}
