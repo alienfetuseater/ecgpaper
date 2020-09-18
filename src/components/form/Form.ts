4891270469e7d3dad2566dcc329b760ea8515c79
alienfetuseater <alienfetuseater@gmail.com>
2020-09-17 23:50:46 -0400

    more breaking of form store

--- src/components/form/Form.ts
+++ src/components/form/Form.ts
@@ -1,10 +1,13 @@
 import { leadStateObject, formFeatureStateObject, Lead } from 'interfaces'
 import Select from './Select'
+
 export default function sideForm(
 	leadStore: leadStateObject[],
 	formStore: Map<string, formFeatureStateObject>,
 ): { init: void } {
 	const main = document.querySelector('main')
+
+	// this proxy needs to be reconfigured to hold the formStateObject for the lead selected
 	const leadProxy = new Proxy(
 		{
 			leadIndex: undefined,
@@ -42,29 +45,19 @@ export default function sideForm(
 	const form = (): HTMLFormElement => {
 		const form = document.createElement('form')
 
-		const leadLabel = document.createElement('label')
-		leadLabel.setAttribute('for', 'lead-select')
-		leadLabel.textContent = 'select lead you wish to edit'
-		form.appendChild(leadLabel)
-		const leadSelect = Select(leadStore, leadProxy)
+		const leadSelectLabel = document.createElement('label')
+		leadSelectLabel.setAttribute('for', 'lead-select')
+		leadSelectLabel.textContent = 'select lead you wish to edit'
+		form.appendChild(leadSelectLabel)
 
-		/**
-		 * everytime the lead is changed, a call is made to the form state
-		 * to load all the values for that lead into the form values
-		 */
+		const leadSelect = Select(formStore, leadProxy)
 		leadSelect.addEventListener('change', (e: Event) => {
-			const lead = (e.target as HTMLSelectElement).value
+			const leadSelected = (e.target as HTMLSelectElement).value
+
+			// when changing a lead, this resets the value to the native value of that lead
 			const inputs = document.querySelectorAll('input')
-
-			console.log(lead)
 			inputs.forEach((input, index) => {
-				if (lead === undefined) {
-					input.setAttribute('disabled', '')
-				} else {
-					input.removeAttribute('disabled')
-				}
-
-				input.value = String(formStore[index].value)
+				input.value = String(formStore.get(leadSelected)[index].value)
 				// load diffs from formState for selected leads values
 			})
 		})
@@ -73,69 +66,77 @@ export default function sideForm(
 		const fieldSet = document.createElement('fieldset')
 		const legend = document.createElement('legend')
 		legend.textContent = 'lead: undefined'
-		fieldSet.appendChild(legend)
-		// iife
-		;((formStore: formFeatureObject[]) => {
-			let featureName: string | undefined = undefined
-			formStore.forEach((el: formFeatureObject) => {
-				const p = document.createElement('p')
-				if (featureName != el.feature) {
-					const h4 = document.createElement('h4')
-					h4.textContent = el.feature
-					p.appendChild(h4)
-				}
-				const label = document.createElement('label')
-				label.textContent = el.characteristic
-				label.setAttribute(
-					'for',
-					`${el.feature.concat(el.characteristic)}`,
-				)
-				const input = document.createElement('input')
-				input.setAttribute('type', 'range')
-				input.setAttribute('name', `${el.feature}.${el.characteristic}`)
-				input.setAttribute(
-					'id',
-					`${el.feature.concat(el.characteristic)}`,
-				)
-				input.setAttribute('max', String(el.max))
-				input.setAttribute('min', String(el.min))
-				input.setAttribute('value', String(el.value))
-				input.setAttribute('disabled', '')
 
-				input.addEventListener('change', (e: Event) => {
-					switch (leadProxy.leadName) {
-						case 'undefined':
-							console.log('must pick a lead first')
-							break
-						case 'global':
-							leadStore.forEach((stateObject) => {
-								for (const property in stateObject) {
+		fieldSet.appendChild(legend)
+			/**
+			 * iife for generating form fields
+			 * 
+			 * need to reconfigure to generate formFields on load
+			 * with respect to lead 1
+			 * and then the event listener will change the values of the 
+			 * fields when new leads are selected
+			 */
+			; ((formStore: Map<string, formFeatureStateObject>) => {
+				let featureName: string | undefined = undefined
+
+				formStore.forEach((el: formFeatureObject) => {
+					const p = document.createElement('p')
+					if (featureName != el.feature) {
+						const h4 = document.createElement('h4')
+						h4.textContent = el.feature
+						p.appendChild(h4)
+					}
+					const label = document.createElement('label')
+					label.textContent = el.characteristic
+					label.setAttribute(
+						'for',
+						`${el.feature.concat(el.characteristic)}`,
+					)
+					const input = document.createElement('input')
+					input.setAttribute('type', 'range')
+					input.setAttribute('name', `${el.feature}.${el.characteristic}`)
+					input.setAttribute(
+						'id',
+						`${el.feature.concat(el.characteristic)}`,
+					)
+					input.setAttribute('max', String(el.max))
+					input.setAttribute('min', String(el.min))
+					input.setAttribute('value', String(el.value))
+
+					input.addEventListener('change', (e: Event) => {
+						switch (leadProxy.leadName) {
+							case 'undefined':
+								console.log('must pick a lead first')
+								break
+							case 'global':
+								leadStore.forEach((stateObject) => {
+									for (const property in stateObject) {
+										if (property === input.id) {
+											stateObject[property] = input.value
+										}
+									}
+								})
+								break
+							default:
+								for (const property in leadStore[
+									Number(leadProxy.leadIndex)
+								]) {
 									if (property === input.id) {
-										stateObject[property] = input.value
+										leadStore[Number(leadProxy.leadIndex)][
+											property
+										] = Number(input.value)
 									}
 								}
-							})
-							break
-						default:
-							for (const property in leadStore[
-								Number(leadProxy.leadIndex)
-							]) {
-								if (property === input.id) {
-									leadStore[Number(leadProxy.leadIndex)][
-										property
-									] = Number(input.value)
-								}
-							}
-							break
-					}
-				})
+								break
+						}
+					})
 
-				label.appendChild(input)
-				p.appendChild(label)
-				fieldSet.appendChild(p)
-				featureName = el.feature
-			})
-		})(formStore) // so here formStore will be an array and we will need to pass an index based on the selected lead
+					label.appendChild(input)
+					p.appendChild(label)
+					fieldSet.appendChild(p)
+					featureName = el.feature
+				})
+			})(formStore) // so here formStore will be an array and we will need to pass an index based on the selected lead
 		form.appendChild(fieldSet)
 
 		return form
--- src/components/form/Select.ts
+++ src/components/form/Select.ts
@@ -1,30 +1,27 @@
-import { leadStateObject, Lead } from 'interfaces'
+import { formFeatureStateObject, Lead } from 'interfaces'
 
 export default function Select(
-	store: leadStateObject[],
+	store: Map<string, formFeatureStateObject>,
 	leadProxy: Lead,
 ): HTMLSelectElement {
 	const select = document.createElement('select')
 	select.setAttribute('id', 'lead-select')
 
-	const option = document.createElement('option')
-	option.setAttribute('value', undefined)
-	option.setAttribute('label', '--Please Select A Lead--')
-	select.appendChild(option)
-
-	const optionTwo = document.createElement('option')
-	optionTwo.setAttribute('value', 'global')
-	optionTwo.setAttribute('label', 'global')
-	select.appendChild(optionTwo)
-
-	store.forEach((lead, index) => {
+	store.forEach((value, key) => {
 		const option = document.createElement('option')
-		option.setAttribute('value', String(index))
-		option.setAttribute('label', lead.lead)
+		option.setAttribute('value', key)
+		option.setAttribute('label', key)
 		select.appendChild(option)
 	})
+
+	const option = document.createElement('option')
+	option.setAttribute('value', 'global')
+	option.setAttribute('label', 'global')
+	select.appendChild(option)
+
 	select.addEventListener('change', (e: Event) => {
-		leadProxy.leadIndex = (e.target as HTMLSelectElement).value
+		// leadProxy.leadIndex = (e.target as HTMLSelectElement).value
+		return store.get((e.target as HTMLSelectElement).value)
 	})
 	return select
 }
